01b2df496d2fc792e14dd47cb6c29c46ff5e8de7
Enmanuel Domínguez <manuelx_05@hotmail.com>
2021-11-24 12:14:57 -0400

    routes

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,32 +1,21 @@
-import React from 'react'
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Routes as RouteList, Route } from "react-router-dom";
 import Home from "../container/Home/Home";
 import UseEffectUpdate from "../container/UseEffectUpdate/UseEffectUpdate";
 import RenderCondicional from "../container/RenderCondicional/RenderCondicional";
-import RenderLista from '../container/RenderLista/RenderLista';
-import RenderSinLista from '../container/RenderSinLista/RenderSinLista';
+import RenderLista from "../container/RenderLista/RenderLista";
+import RenderSinLista from "../container/RenderSinLista/RenderSinLista";
 
 function Routes() {
-    return (
-        
-        <Switch>
-          <Route path="/rendersinlista">
-            <RenderSinLista />
-          </Route>
-          <Route path="/renderlista">
-            <RenderLista />
-          </Route>
-          <Route path="/rendercondicional">
-            <RenderCondicional />
-          </Route>
-          <Route path="/UseEffectUpdate">
-            <UseEffectUpdate />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    )
+  return (
+    <RouteList>
+      <Route path="/rendersinlista" component={RenderSinLista} />
+      <Route path="/renderlista" component={RenderLista} />
+      <Route path="/rendercondicional" component={RenderCondicional} />
+      <Route path="/UseEffectUpdate" component={UseEffectUpdate} />
+      <Route path="/" component={Home} />
+    </RouteList>
+  );
 }
 
 export default Routes;
