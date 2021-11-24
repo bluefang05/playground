11c7d4d1a389a67891e714eb678703f6095eb7bd
Enmanuel Domínguez <manuelx_05@hotmail.com>
2021-11-24 10:51:16 -0400

    testing package json config for netlify

--- src/container/UseEffectUpdate/UseEffectUpdate.js
+++ src/container/UseEffectUpdate/UseEffectUpdate.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import "./UseEffectUpdate.css";
+import CodeBlock from '@tenon-io/tenon-codeblock';
+
 function UseEffectUpdate() {
     let [count, setCount] = useState(0);
     let addOne = function () {  
@@ -21,9 +23,44 @@ function UseEffectUpdate() {
             {[...Array(count)].map( function (el, i) { 
             return <div className="ball"></div> })}
             </div>
-
+            <CodeBlock codeString = {code} />
+            
         </div>
     )
 }
 
 export default UseEffectUpdate
+
+var code = `
+import React, {useEffect, useState} from 'react'
+import "./UseEffectUpdate.css";
+import CodeBlock from '@tenon-io/tenon-codeblock';
+
+function UseEffectUpdate() {
+    let [count, setCount] = useState(0);
+    let addOne = function () {  
+        setCount(count + 1);
+    }
+
+    useEffect(() => {
+        
+        alert(count);
+    },[count])
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={addOne}>add 1</button>
+            UseEffectUpdate
+
+            <div id="ballContainer">
+            {[...Array(count)].map( function (el, i) { 
+            return <div className="ball"></div> })}
+            </div>
+        </div>
+    )
+}
+
+export default UseEffectUpdate
+`;
+
